ab3d1a349b60f1095bfdb9c29b3de5daeecf2b7c
Luke Moderwell <luke.moderwell@gmail.com>
2018-09-18 16:57:54 -0400

    testing critical css bundle for separate pages

--- _assets/javascript/index.js
+++ _assets/javascript/index.js
@@ -1,1 +1,26 @@
 // JS
+// Find all classes above the fold
+
+function findClasses() {
+  let classes = [];
+  const criticalContent = document.querySelector('[data-critical-content]');
+  
+  // add parent classes
+  const parentClasses = criticalContent.classList.value.split(' ');
+  for (var i = 0; i < parentClasses.length; i += 1) {
+    classes.push(parentClasses[i]);
+  }
+
+  // add child classes
+  const children = criticalContent.children;
+  for (var x = 0; x < children.length; x += 1) {
+    var childClasses = children[x].classList.value.split(' ');
+    for (var z = 0; z < childClasses.length; z += 1) {
+      classes.push(childClasses[z]);
+    }
+  }
+
+  console.log(classes);
+}
+
+findClasses();
--- _plugins/hooks/critical.rb
+++ _plugins/hooks/critical.rb
@@ -1,6 +1,5 @@
 Jekyll::Hooks.register(:site, :post_write) do |_site|
-  # Before we begin building, delete any lingering CSS files. This ensures we
-  # don't purge the incorrect file.
+  # Before we begin building, find critical css classes
   # Dir.glob('_site/assets/*.css').each { |f| File.delete(f) }
 end
 
